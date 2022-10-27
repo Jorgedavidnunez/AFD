2d8d23c2539ea5fef19d1500a5854464d10f3480
jorgedavidnunez <dingox57@gmail.com>
2022-10-27 17:12:35 -0600

    AFD

---
+++ AFD/src/afd/AFD.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package afd;
+
+/**
+ *
+ * @author david
+ */
+public class AFD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frm_interfaz frm = new frm_interfaz();
+        frm.show();
+    }
+    
+}
